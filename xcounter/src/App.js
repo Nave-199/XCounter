842c8e2d08b4b62acf5a26996657add1665b5072
NaVeEN98801 <naveenn199999@gmail.com>
2024-07-25 16:56:11 +0530

    Changes done

--- xcounter/src/App.js
+++ xcounter/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setcount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setcount(count + 1)}>Increment</button>
+      <button onClick={() => setcount(count - 1)}>Decrement</button>
     </div>
   );
 }
